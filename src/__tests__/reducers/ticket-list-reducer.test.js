416fea262dbb859f8f68e49475b8b023d762f83c
Samuel Majerus <ladolego@gmail.com>
2022-11-28 10:04:47 -0800

    Adds second Redux test.   [End of code-along work for Lsn 5.]
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,8 +1,37 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
+
+  let action;
+  const ticketData = {
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, {type: null})).toEqual({});
+  });
+
+  test("Should successfully add new ticket data to mainTicketList", () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
   });
 
 });
