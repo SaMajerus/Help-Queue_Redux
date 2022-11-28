cb4bde8c29989e77921f9807c57f970ed49131c2
Samuel Majerus <ladolego@gmail.com>
2022-11-28 09:37:03 -0800

    Adds first Jest test.    [End of code-along work for Lsn 4.]

---
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,8 @@
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+describe('ticketListReducer', () => {
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(ticketListReducer({}, {type: null})).toEqual({});
+  });
+
+});
