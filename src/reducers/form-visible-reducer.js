d2bafcad0879487b0ba989367123636910e0326e
Samuel Majerus <ladolego@gmail.com>
2022-11-28 16:21:52 -0800

    Adds Root Reducer, along with its corresponding suite of Jest tests.  [End of code-along work for Lsn 18.]
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

---
+++ src/reducers/form-visible-reducer.js
@@ -0,0 +1,10 @@
+const reducer = (state = false, action) => {
+  switch (action.type) {
+    case 'TOGGLE_FORM':
+      return !state;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
